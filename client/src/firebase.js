f3a65fb0788c83be8ad310558bf9d5dea0275f38
Moses Udoka <UdokaMosesAugustus>
2026-07-03 10:49:20 +0100

    fix firebase env vars

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,9 +2,9 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDvNB8vWT4HzkcbErKL1haPY05032uCOlc",
-  authDomain: "camerino-ffd39.firebaseapp.com",
-  projectId: "camerino-ffd39",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: "camerino-ffd39.firebasestorage.app",
   messagingSenderId: "1051145603468",
   appId: "1:1051145603468:web:05e8f57085318f2b81aea9"
